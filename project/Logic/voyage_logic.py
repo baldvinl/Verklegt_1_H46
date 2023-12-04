317ae84a2cec75f84920208354422ec91ed07027
sarafells <144339992+sarafells@users.noreply.github.com>
2023-12-04 21:20:20 +0000

    Update voyage_logic.py

---
+++ project/Logic/voyage_logic.py
@@ -0,0 +1,21 @@
+from data.voyage_data import Voyage_Data
+from model.voyage import Voyage
+
+class Voyage_Logic:
+    def __init__(self, data_connection):
+        pass
+
+    def register_voyage(self, voyage):
+        pass
+
+        self.data_wrapper.register_voyage(voyage)
+
+    def get_information(self, ):
+        pass
+
+        self.data_wrapper.get_information()
+
+    def add_crew(self, ):
+        pass
+
+        self.data_wrapper.add_crew()
