6cda2803e173dfb542653fd0ba608d1ee67e8e22
Gunnsan <95713284+Gunnsan@users.noreply.github.com>
2023-12-04 20:15:14 +0000

    model classes defined

---
+++ project/Model/aircraft.py
@@ -0,0 +1,6 @@
+class Aircraft:
+    def __init__(self, name='Aircraft', type='123', manufacturer='Focker', seatcount='20'):
+        self.name = name
+        self.type = type
+        self.manufacturer = manufacturer
+        self.seatcount = seatcount
---
+++ project/Model/crew.py
@@ -0,0 +1,9 @@
+class Crew:
+    def __init__(self, ssn='9999999999', name='Chuck Norris', address='Beststreet 1', area_code='999', email='chucknorris@ru.is', mobile_no='9999999', phone_no=None):
+        self.ssn = ssn
+        self.name = name
+        self.address = address
+        self.area_code = area_code
+        self.email = email
+        self.mobile_no = mobile_no
+        self.phone_no = phone_no
