397c43bfeefb055021d733d1e3eb24fba1c9148d
Gunnsan <95713284+Gunnsan@users.noreply.github.com>
2023-12-05 19:38:19 +0000

    destination_data update

--- project/data/data_wrapper.py
+++ project/data/data_wrapper.py
@@ -1,6 +1,5 @@
 from data.destination_data import Destination_Data
 
-
 class Data_Wrapper:
     def __init__(self):
         self.destination_data = Destination_Data()
--- project/gudrunelin_debug.py
+++ project/gudrunelin_debug.py
@@ -1,6 +1,10 @@
 from data.destination_data import Destination_Data
+from model.destination import Destination
 
+temp = Destination()
 data_class = Destination_Data()
+data_class.create_destination(temp)
 result = data_class.display_destinations()
 for elem in result:
     print(elem.country, elem.airport, elem.distance)
+
