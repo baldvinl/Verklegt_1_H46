643bf7dfe07d53c16ecfb592803192222f9d508a
Gunnsan <95713284+Gunnsan@users.noreply.github.com>
2023-12-05 17:27:55 +0000

    ..

--- project/Data/data_wrapper.py
+++ project/Data/data_wrapper.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 from data.destination_data import Destination_Data
 
-=======
-from project.data.destination_data import Destination_Data
->>>>>>> project
 
 class Data_Wrapper:
     def __init__(self):
--- project/Data/destination_data.py
+++ project/Data/destination_data.py
@@ -1,8 +1,5 @@
 import csv
-<<<<<<< HEAD
 from model.destination import Destination
-=======
->>>>>>> project
 
 class Destination_Data:
     def __init__(self):
@@ -15,10 +12,7 @@ class Destination_Data:
         dest_list = []
         with open(self.filename, newline='', encoding="utf-8") as csvfile:
             reader = csv.DictReader(csvfile)
-<<<<<<< HEAD
             for line in reader:
                 dest_list.append(Destination(line["country"], line["IATA"], line["flight_duration"], 
                                              line["distance"], line["ice_name"], line["ice_number"]))
         return dest_list
-=======
->>>>>>> project
