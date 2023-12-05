a3f393c4a2728a04a72c5df9ac89ea7fafd10e5a
Gunnsan <95713284+Gunnsan@users.noreply.github.com>
2023-12-05 23:30:36 +0000

    setting up all data files

---
+++ project/data/crew_data.py
@@ -0,0 +1,5 @@
+import csv
+
+class Crew_Data:
+    def __init__(self):
+        self.file_name = "files/crew.csv"
--- project/data/data_wrapper.py
+++ project/data/data_wrapper.py
@@ -1,8 +1,14 @@
 from data.destination_data import Destination_Data
+from project.data.aircraft_data import Aircraft_Data
+from project.data.crew_data import Crew_Data
+from project.data.voyage_data import Voyage_Data
 
 class Data_Wrapper:
     def __init__(self):
         self.destination_data = Destination_Data()
+        self.crew_data = Crew_Data()
+        self.aircraft_data = Aircraft_Data()
+        self.voyage_data = Voyage_Data()
         
     def display_destinations(self):
         return self.destination_data.display_destinations()
