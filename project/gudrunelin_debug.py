584ce19174cc5019a814af4138b02f83fdd50840
Gunnsan <95713284+Gunnsan@users.noreply.github.com>
2023-12-06 17:43:43 +0000

    ..

---
+++ project/gudrunelin_debug.py
@@ -0,0 +1,10 @@
+# from data.destination_data import Destination_Data
+# from model.destination import Destination
+
+# temp = 'NEW NUMBER'
+# iata = 'IATA'
+# data_class = Destination_Data()
+# data_class.change_ice_number(iata, temp)
+# result = data_class.display_destinations()
+# for elem in result:
+#     print(elem.country, elem.airport, elem.ice_name, elem.ice_number)
