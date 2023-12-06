895fb1d32a252cdbf877a890e2c621e4e30bad5e
Gunnsan <95713284+Gunnsan@users.noreply.github.com>
2023-12-06 22:34:01 +0000

    update on destination_data and model

--- project/data/voyage_data.py
+++ project/data/voyage_data.py
@@ -1,5 +1,18 @@
 import csv
+from model.voyage import Voyage
 
 class Voyage_Data:
     def __init__(self):
         self.file_name = "files/voyage.csv"
+
+    def create_voyage(self, voyage):
+        """Adds a new voyage to the file"""
+
+        with open(self.file_name, 'a', newline='', encoding="utf-8") as csvfile:
+            fieldnames = ["destination", "date", "time_depart_iceland", "time_depart_destination"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writerow({"destination": voyage.destination, "date": voyage.date, "time_depart_iceland": voyage.time_depart_iceland, 
+                             "time_depart_destination": voyage.time_depart_destination})
+            
+    
