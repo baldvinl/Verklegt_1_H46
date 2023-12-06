c7c9e2bd64bdb2b48542667d048a85ef0f5d6717
Gunnsan <95713284+Gunnsan@users.noreply.github.com>
2023-12-06 18:33:35 +0000

    ..

--- project/model/flight_attendant.py
+++ project/model/flight_attendant.py
@@ -1,5 +1,6 @@
 from project.model.crew import Crew
 
+
 class Flight_Attendant(Crew):
     def __init__(self, ssn='9999999999', name='Chuck Norris', address='Beststreet 1', 
                  area_code='999', email='chucknorris@ru.is', mobile_no='9999999', phone_no=None):
