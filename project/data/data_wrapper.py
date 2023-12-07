e786a38cd8ba5771642e63f438de26adc22e68c8
Gunnsan <95713284+Gunnsan@users.noreply.github.com>
2023-12-07 21:22:04 +0000

    crew data update

--- project/data/data_wrapper.py
+++ project/data/data_wrapper.py
@@ -11,8 +11,9 @@ class Data_Wrapper:
         self.aircraft_data = Aircraft_Data()
         self.voyage_data = Voyage_Data()
 
-        #destination functions
-        
+
+#destination functions
+
     def display_destinations(self):
         return self.destination_data.display_destinations()
     
@@ -25,17 +26,26 @@ class Data_Wrapper:
     def change_ice_number(self, iata, new_ice_number):
         return self.destination_data.change_ice_name(iata, new_ice_number)
     
-    #aircraft functions
+
+#crew functions
+
+    def register_pilot(self, pilot):
+        return self.crew_data.register_pilot(pilot)
     
-    def create_aircraft(self, aircraft):
-        return self.aircraft_data.create_aircraft(aircraft)
+    def register_flight_attendant(self, flight_attendant):
+        return self.crew_data.register_flight_attendant(flight_attendant)
     
-    #Voyage functions
+
+#Voyage functions
 
     def create_voyage(self, voyage):
         return self.voyage_data.create_voyage(voyage)
-
-    #crew functions
-
     
+
+#aircraft functions
+    
+    def create_aircraft(self, aircraft):
+        return self.aircraft_data.create_aircraft(aircraft)
+
+
     
