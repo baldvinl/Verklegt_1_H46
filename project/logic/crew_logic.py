a1c0d96def0b8e1978de3bd308a80a961eb60633
baldvinl <139862929+baldvinl@users.noreply.github.com>
2023-12-07 21:49:15 +0000

    Crew logic fix

--- project/logic/crew_logic.py
+++ project/logic/crew_logic.py
@@ -1,15 +1,17 @@
-from data.crew_data import Crew_Data
+from data.data_wrapper import Data_Wrapper
 from model.crew import Crew
 from model.pilot import Pilot
 from model.flight_attendant import Flight_Attendant
 
+
 class Crew_Logic:
-    def __init__(self, data_connection):
-        self.data_wrapper = data_connection
+    def __init__(self):
+        self.data_wrapper = Data_Wrapper()
 
     def register_crew(self, crew):
         """Checks if crew object received is of the type 
         Pilot or not and forwards to data wrapper accordingly"""
+
         if isinstance(crew, Pilot):
             return self.data_wrapper.register_pilot(crew)
         else:
@@ -20,24 +22,30 @@ class Crew_Logic:
 
     def display_all_crew(self):
         """Forwards request to data wrapper"""
+
         return self.data_wrapper.display_all_crew()
 
     def get_crew_info(self, ssn):
         """Receives social security number of crew member and forwards to data wrapper"""
+        
         return self.data_wrapper.get_crew_info(ssn)
 
     def display_not_working(self, date):
         """Receives date and forwards to data wrapper"""
+        
         return self.data_wrapper.display_not_working(date)
 
     def display_working(self, date):
         """Receives date and forwards to data wrapper"""
+        
         return self.data_wrapper.display_working(date)
     
     def display_pilots(self):
         """Forwards request to data wrapper"""
+        
         return self.data_wrapper.display_pilots()
     
     def display_flight_attendants(self):
         """Forwards requests to data wrapper"""
+        
         return self.data_wrapper.display_flight_attendants()
