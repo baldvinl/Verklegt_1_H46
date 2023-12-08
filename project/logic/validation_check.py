36131928c4e2e3e2bc8c512667465d8772c31058
georgiamal <georgia23@ru.is>
2023-12-08 17:48:56 +0000

    comments

--- project/logic/validation_check.py
+++ project/logic/validation_check.py
@@ -1,10 +1,35 @@
 
-# TO ADD
-# - Crew
-    #
-# - Aircraft
-    #
-# - Destination
-    #
-# - Voyage
-    #
+class Validation:
+
+    # Crew
+        # Name [just letters 3-30 - no numbers or symbol] -- maybe split functions for first and last name
+        # SSN [just digits up to 10]
+        # Phone number [just digits up to 15]
+        # Address [just numbers and letters, dashes, spaces]
+        # Landline -- same as phone number probably 
+        # Email first part cant contain ".", one "@" only, second part needs to contain "."
+
+    # Aircraft
+        # Name [2 letters 3 numbers]
+        # Type
+        # Manufacturer
+        # seat count
+        # status
+
+    # Destination
+        # Country
+        # Airport
+        # Flight time
+        # Flight duration
+        # Distance from iceland
+        # emergency contact name
+        # number
+
+    # Voyage
+        # Destination
+        # flight to
+        # flight back
+        # fully manned
+        # status
+        # pilot count
+        # f.a count
