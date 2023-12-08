9880fb43274b812f750d80e08b3e46002bfc4f38
georgiamal <georgia23@ru.is>
2023-12-08 18:12:59 +0000

    skeleton of some functions

--- project/logic/validation_check.py
+++ project/logic/validation_check.py
@@ -1,5 +1,7 @@
 
 class Validation:
+    def __init__(self) -> None:
+        pass
 
     # Crew
         # Name [just letters 3-30 - no numbers or symbol] -- maybe split functions for first and last name
@@ -9,12 +11,30 @@ class Validation:
         # Landline -- same as phone number probably 
         # Email first part cant contain ".", one "@" only, second part needs to contain "."
 
+    def validate_name(self, name):
+        pass
+
+    def validate_ssn(self, ssn):
+        pass
+
+    def validate_phone_number(self, number):
+        pass
+
+    def validate_address(self, address):
+        pass
+
+    def validate_email(self, email):
+        pass
+
     # Aircraft
         # Name [2 letters 3 numbers]
-        # Type
-        # Manufacturer
-        # seat count
+        # Type 
+        # Manufacturer [probably use validate name?]
+        # seat count 
         # status
+
+    def validate_aircraft_name(self, name):
+        pass
 
     # Destination
         # Country
