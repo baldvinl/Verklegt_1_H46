372737471cdda0f5b5749ee916ba6b8607816401
georgiamal <georgia23@ru.is>
2023-12-09 18:41:49 +0000

    fixed merge error

--- project/model/voyage.py
+++ project/model/voyage.py
@@ -1,6 +1,4 @@
 class Voyage:
-    def __init__(self, destination='destination', time_depart_iceland = 'dd/mm/yy 00:00', time_depart_destination = 'dd/mm/yy 00:00', captain = '', pilot = '', head_flight_attendant = ''):
-        self.destination = destination
     def __init__(self, destination='destination', time_depart_iceland = 'dd/mm/yy 00:00', time_depart_destination = 'dd/mm/yy 00:00', 
                  captain = '', pilot = '', head_flight_attendant = '', flight_attendant1 ='', flight_attendant2=''):
         self.destinastion = destination
